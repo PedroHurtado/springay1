5ecced4063baf835588db9ece5bc84448f34a854
Pedro Hurtado <pedrohurt@gmail.com>
2025-05-29 13:29:10 +0200

    Query and entityManager

--- src/main/java/com/example/demo/features/ingredient/IngredientCreate.java
+++ src/main/java/com/example/demo/features/ingredient/IngredientCreate.java
@@ -4,10 +4,15 @@ import java.util.UUID;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Ingredient;
 import com.example.demo.infraestructure.IngredientRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,15 +45,19 @@ public class IngredientCreate {
     }
     @org.springframework.stereotype.Service
     public class ServiceImpl implements Service{
-
+        
+        @PersistenceContext
+        private EntityManager entityManager;
         private final IngredientRepository repository;
         public ServiceImpl(IngredientRepository repository){
             this.repository = repository;
         }
         @Override
+        @Transactional
         public Response handler(Request req) {
-            var ingredient= Ingredient.create(UUID.randomUUID(), req.name(), req.cost());
-            repository.save(ingredient);            
+            var ingredient= Ingredient.create(UUID.randomUUID(), req.name(), req.cost());            
+            //repository.save(ingredient);            
+            entityManager.persist(ingredient);
             return new Response(ingredient.getId(), ingredient.getName(), ingredient.getCost());
         }
 
--- src/main/java/com/example/demo/features/ingredient/query/QueryIngredient.java
+++ src/main/java/com/example/demo/features/ingredient/query/QueryIngredient.java
@@ -1,33 +1,46 @@
 package com.example.demo.features.ingredient.query;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.infraestructure.IngredientRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Configuration
 public class QueryIngredient {
     
-    public record Response(UUID id,String name, double cost) {
+    public record Response(UUID id,String name) {
     }
     @RestController
     public class Controller{        
+        private final IngredientRepository repository;
         
+        public Controller(final IngredientRepository repository) {
+            this.repository = repository;
+        }
+
         @GetMapping("/ingredients")
         public ResponseEntity<?> hander(
             @RequestParam(required = false) String name,
-            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "25") int size, 
             @RequestHeader(name="dni", defaultValue = "52803357A") String dni
         ){
-            List<Response> list = new ArrayList<>();
-            return ResponseEntity.ok().body(list);
+            var ingredients =  repository.findByCritereria(
+                name, PageRequest.of(page, size)
+            ).stream(). map(i->new Response(i.getId(), i.getName()))
+            //.collect(Collectors.toList())
+            ;
+
+            return ResponseEntity.ok().body(ingredients);
         }
     }
 }
